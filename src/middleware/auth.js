5f5d9c2f01f50fa24590866acadc52729d8a0a9e
Bui Manh <buimanh17112001@gmail.com>
2021-04-01 04:09:25 +0700

    manh

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,18 +2,22 @@ const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
   let token = req.cookies.userToken
-  if (token) {
-    jwt.verify(token, 'mb1o4er', (err, result) => {
-      if (err) {
-        req.err = 'khong the xac thuc'
-        return next('last')
-      }
-      req.userId = result._id
-      next()
-    })
-  } else {
-    req.err = token
-    next('last')
-  }
+  res.json({
+    status: false,
+    message: `this is ${token}`
+  })
+  // if (token) {
+  //   jwt.verify(token, 'mb1o4er', (err, result) => {
+  //     if (err) {
+  //       req.err = 'khong the xac thuc'
+  //       return next('last')
+  //     }
+  //     req.userId = result._id
+  //     next()
+  //   })
+  // } else {
+  //   req.err = `err: ${token}`
+  //   next('last')
+  // }
 
 }
