64808db8301b8098cb730afca0ce5a531e66c2f6
Bui Manh <buimanh17112001@gmail.com>
2021-04-01 03:54:11 +0700

    manh

--- src/apis/controllers/SignUpController.js
+++ src/apis/controllers/SignUpController.js
@@ -11,26 +11,31 @@ class SignUnController {
     AccountModel.findOne({ username })
       .then((dataRes, something) => {
         if (!dataRes) {
-          
+
           newAccount.save((err) => {
             if (err === null) {
               let id = newAccount._id
               let image = newAccount.image
               let username = newAccount.username
               let password = newAccount.password
-              let token = jwt.sign({ _id: id, username: username, password }, 'mb1o4er') || null
-
-              res.json({
-                logged: true,
-                userData: {
-                  firstName: newAccount.firstName || null,
-                  lastName: newAccount.lastName || null,
-                  id,
-                  image,
-                  bio: newAccount.bio
-                },
-                token
+              jwt.sign({ _id: id, username: username, password }, 'mb1o4er', (err, token) => {
+                if (err) {
+                  req.err = 'loi sign token'
+                  return next('last')
+                }
+                res.json({
+                  logged: true,
+                  userData: {
+                    firstName: newAccount.firstName || null,
+                    lastName: newAccount.lastName || null,
+                    id,
+                    image,
+                    bio: newAccount.bio
+                  },
+                  token
+                })
               })
+
             }
           })
         } else {
