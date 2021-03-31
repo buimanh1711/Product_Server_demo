048fbf627e25fd24e97c56bc09b513a3eda93ec4
Bui Manh <buimanh17112001@gmail.com>
2021-04-01 04:45:30 +0700

    manh

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res, next) => {
+  console.log(req)
   let token = req.cookies
   console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', req.cookies)
   res.json({
