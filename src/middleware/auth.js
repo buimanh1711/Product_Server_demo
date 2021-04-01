0ea504350bc3e86ee6ea6f4ae3982463892805d3
Bui Manh <buimanh17112001@gmail.com>
2021-04-01 12:58:29 +0700

    manh

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 
-module.exports = (req, res, next) => {
+const auth = (req, res, next) => {
+  console.log('this is all cookies: ', req.cookies)
   let token = req.cookies ? req.cookies.userToken : null
   if (token) {
     jwt.verify(token, 'mb1o4er', (err, result) => {
@@ -15,5 +16,6 @@ module.exports = (req, res, next) => {
     req.err = `err: ${token}`
     next('last')
   }
-
 }
+
+module.exports = auth
