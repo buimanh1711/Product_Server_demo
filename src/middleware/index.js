b0f31fc329958e2c90690a4d7c18ed8785f94fb9
Bui Manh <buimanh17112001@gmail.com>
2021-04-01 15:34:38 +0700

    manh

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -6,7 +6,7 @@ const fileUpload = require('express-fileupload')
 const cors = require('cors')
 
 const middleware = (app) => {
-  app.use(cors({ origin: 'https://mb1o4er.herokuapp.com', preflightContinue: true, credentials: true, methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS" }))
+  app.use(cors({ origin: 'http://localhost:3999', preflightContinue: true, credentials: true, methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS" }))
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
   app.use(cookieParser())
